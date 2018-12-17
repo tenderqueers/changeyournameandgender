cd8e25f76e18e403364a70f8e1fb96ac95d1041c
Zak Auerbach <zak@codeforamerica.org>
2018-12-16 18:38:54 -0800

    I think I'm getting the hang of this redux thing

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { dispatchAnswer } from '../lib/store';
-import Router from 'next/router';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Input, InputGroup, InputGroupAddon } from 'reactstrap';
+import { Button } from 'reactstrap';
+import { QuestionCalifornia, QuestionName, QuestionHaveCaliforniaBirthCertificate } from '../lib/questions';
 
 class Index extends Component {
   constructor(props) {
     super(props)
 
     this.questions = Array(4).fill(null)
-    this.questions[0] = <QuestionCalifornia dispatch={this.props.dispatch} />
-    this.questions[1] = <QuestionName dispatch={this.props.dispatch} />
+    this.questions[0] = <QuestionCalifornia />
+    this.questions[1] = <QuestionName />
     this.questions[2] = <QuestionHaveCaliforniaBirthCertificate />
     // this.questions[3] = <QuestionNewCaliforniaBirthCertificate onClick={(answer) => this.saveAnswer(answer)}/>
 
@@ -44,69 +43,6 @@ class Index extends Component {
           </ul>
         </div>
       </div>
-    )
-  }
-}
-
-class QuestionCalifornia extends Component {
-  answerYes() {
-    const {dispatch} = this.props
-    dispatchAnswer('california', true)(dispatch)
-    Router.push({pathname: '/', query: { question: 1 }})
-  }
-
-  answerNo() {
-    const {dispatch} = this.props
-    Router.push({pathname: '/notAvailable'})
-    dispatchAnswer('california', false)(dispatch)
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Let's get started!</h1>
-        <h2>Are you currently living in California?</h2>
-        <Button color="danger" onClick={() => { this.answerNo() }}>No</Button>
-        <Button color="success" onClick={() => { this.answerYes() }}>Yes</Button>
-      </div>
-    )
-  }
-}
-
-class QuestionName extends Component {
-  handleChange(event) {
-    const {dispatch} = this.props
-    dispatchAnswer('name', event.target.value)(dispatch)
-  }
-
-  answer() {
-    Router.push({pathname: '/', query: { question: 2 }})
-  }
-
-  render() {
-    return (
-      <div>
-        <h2>What's your full current legal name (the name on your Driver's License or Passport)</h2>
-        <InputGroup>
-          <Input placeholder="Miriam Maisel" value={this.props.name} ref="Name" onChange={(e) => this.handleChange(e)} />
-          <InputGroupAddon addonType="append">
-            <Button color="success" onClick={() => { this.answer() }}>Save</Button>
-          </InputGroupAddon>
-        </InputGroup>
-      </div>
-    )
-  }
-}
-
-class QuestionHaveCaliforniaBirthCertificate extends Component {
-  render() {
-    return (
-      <p>this is as far as I've gotten :)</p>
-      // <div>
-      //   <h2>Do you have a California Birth Certificate?</h2>
-      //   <Button color="danger" onClick={() => this.props.onClick(false)}>No</Button>
-      //   <Button color="success" onClick={() => this.props.onClick(true)}>Yes</Button>
-      // </div>
     )
   }
 }
